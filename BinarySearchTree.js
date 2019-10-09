eb6cb56a74b30e757f709f9b4629be0cc771c072
Sam Walker <jack.nasal@gmail.com>
2019-10-09 12:47:49 -0400

    finished 5

---
+++ BinarySearchTree.js
@@ -0,0 +1,78 @@
+class BinarySearchTree {
+  constructor(key = null, value = null, parent = null) {
+    this.key = key
+    this.value = value
+    this.parent = parent
+    this.left = null
+    this.right = null
+  }
+  insert(item) {
+    if (this.key === null) {
+      this.key = item.key
+    } else if (item.key < this.key) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(item.key, item.value, this)
+      } else {
+        this.left.insert(item)
+      }
+    } else if (item.key > this.key) {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(item.key, item.value, this)
+      } else {
+        this.right.insert(item)
+      }
+    } else if (item.key === this.key) {
+      this.value = item.value
+    }
+  }
+  remove(key) {
+    if (this.key === null) {
+      return null
+    } else if (key < this.key) {
+      this.left.remove(key)
+    } else if (key > this.key) {
+      this.right.remove(key)
+    } else if (key === this.key) {
+      //found the key
+      if (this.left && this.right) {
+        //if there are two children
+        const rightMin = this.right._findMin() //find the smallest key on the right side
+        this.key = rightMin.key
+        this.value = rightMin.value
+        this.right.remove(rightMin.key)
+      } else if (this.left) {
+        //if there is only a left child
+        this.left.parent = this.parent
+        if (this.parent.left === this) this.parent.left = this.left
+        if (this.parent.right === this) this.parent.right = this.left
+      } else if (this.right) {
+        //if there is only a right child
+        this.right.parent = this.parent
+        if (this.parent.left === this) this.parent.left = this.right
+        if (this.parent.right === this) this.parent.right = this.right
+      } else {
+        //if there are no children
+        if (this.parent.left === this) this.parent.left = null
+        if (this.parent.right === this) this.parent.right = null
+      }
+    }
+  }
+
+  find(key) {
+    if (key === this.key) {
+      return this.value
+    } else if (key < this.key) {
+      return this.left.find(key)
+    } else if (key > this.key) {
+      return this.right.find(key)
+    }
+  }
+  _findMin() {
+    if (this.left === null) {
+      return this
+    }
+    return this.left._findMin()
+  }
+}
+
+module.exports = BinarySearchTree
--- drills.js
+++ drills.js
@@ -1,3 +1,5 @@
+const BinarySearchTree = require('./BinarySearchTree')
+
 // 1.
 
 // 3, 5, 6, 8, 11, 12, 14, 15, 17, 18
@@ -11,6 +13,8 @@
 // 17, 18
 // it would return null
 
+
+//3.
 function findBook(lib, dewey, title, start = 0, end = lib.length) {
   while (start < end) {
     let mid = Math.floor((start + end) / 2);
@@ -28,3 +32,71 @@ function findBook(lib, dewey, title, start = 0, end = lib.length) {
   }
   return 'Book not found';
 }
+
+//4.
+// 1)
+// in-order: 14 15 19 25 27 35 79 89 90 91 and 
+// pre-order: 35 25 15 14 19 27 89 79 91 90. 
+// post-order: 14, 19, 15, 27, 25, 79, 90, 91, 89, 35
+
+//post-order: 5, 7, 6, 9, 11, 10, 8
+//pre-order: 8, 6, 5, 7, 10, 9, 11
+
+//5.
+const BST = new BinarySearchTree();
+BST.insert({key: 25, value: null})
+BST.insert({key: 15, value: null})
+BST.insert({key: 50, value: null})
+BST.insert({key: 10, value: null})
+BST.insert({key: 24, value: null})
+BST.insert({key: 35, value: null})
+BST.insert({key: 70, value: null})
+BST.insert({key: 4, value: null})
+BST.insert({key: 12, value: null})
+BST.insert({key: 18, value: null})
+BST.insert({key: 31, value: null})
+BST.insert({key: 44, value: null})
+BST.insert({key: 66, value: null})
+BST.insert({key: 90, value: null})
+BST.insert({key: 22, value: null})
+// console.log(BST)
+
+const preOrder = (tree) => {
+  console.log(tree.key)
+  if (tree.left) {
+    preOrder(tree.left)
+  }
+  if (tree.right) {
+    preOrder(tree.right)
+  }
+}
+const postOrder = (tree) => {
+  if (tree.left) {
+    postOrder(tree.left)
+  }
+  if (tree.right) {
+    postOrder(tree.right)
+  }
+  console.log(tree.key)
+}
+const inOrder = (tree) => {
+  if (tree.left) {
+    inOrder(tree.left)
+  }
+  console.log(tree.key)
+  if (tree.right) {
+    inOrder(tree.right)
+  }
+}
+
+console.log('pre-order')
+preOrder(BST)
+console.log('in-order')
+inOrder(BST)
+console.log('post-order')
+postOrder(BST)
+
+
+
+//25 15 50 10 24 35 70 4 12 18 31 44 66 90 22
+
