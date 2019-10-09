84a8b7e819b6387cd7085fcaba78d7f4dbf48d6d
Hector Valles <hmvalles75@yahoo.com>
2019-10-09 12:05:27 -0700

    6

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,78 +1,79 @@
 class BinarySearchTree {
   constructor(key = null, value = null, parent = null) {
-    this.key = key
-    this.value = value
-    this.parent = parent
-    this.left = null
-    this.right = null
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
   }
   insert(item) {
     if (this.key === null) {
-      this.key = item.key
+      this.key = item.key;
+      this.value = item.value;
     } else if (item.key < this.key) {
       if (this.left === null) {
-        this.left = new BinarySearchTree(item.key, item.value, this)
+        this.left = new BinarySearchTree(item.key, item.value, this);
       } else {
-        this.left.insert(item)
+        this.left.insert(item);
       }
     } else if (item.key > this.key) {
       if (this.right === null) {
-        this.right = new BinarySearchTree(item.key, item.value, this)
+        this.right = new BinarySearchTree(item.key, item.value, this);
       } else {
-        this.right.insert(item)
+        this.right.insert(item);
       }
     } else if (item.key === this.key) {
-      this.value = item.value
+      this.value = item.value;
     }
   }
   remove(key) {
     if (this.key === null) {
-      return null
+      return null;
     } else if (key < this.key) {
-      this.left.remove(key)
+      this.left.remove(key);
     } else if (key > this.key) {
-      this.right.remove(key)
+      this.right.remove(key);
     } else if (key === this.key) {
       //found the key
       if (this.left && this.right) {
         //if there are two children
-        const rightMin = this.right._findMin() //find the smallest key on the right side
-        this.key = rightMin.key
-        this.value = rightMin.value
-        this.right.remove(rightMin.key)
+        const rightMin = this.right._findMin(); //find the smallest key on the right side
+        this.key = rightMin.key;
+        this.value = rightMin.value;
+        this.right.remove(rightMin.key);
       } else if (this.left) {
         //if there is only a left child
-        this.left.parent = this.parent
-        if (this.parent.left === this) this.parent.left = this.left
-        if (this.parent.right === this) this.parent.right = this.left
+        this.left.parent = this.parent;
+        if (this.parent.left === this) this.parent.left = this.left;
+        if (this.parent.right === this) this.parent.right = this.left;
       } else if (this.right) {
         //if there is only a right child
-        this.right.parent = this.parent
-        if (this.parent.left === this) this.parent.left = this.right
-        if (this.parent.right === this) this.parent.right = this.right
+        this.right.parent = this.parent;
+        if (this.parent.left === this) this.parent.left = this.right;
+        if (this.parent.right === this) this.parent.right = this.right;
       } else {
         //if there are no children
-        if (this.parent.left === this) this.parent.left = null
-        if (this.parent.right === this) this.parent.right = null
+        if (this.parent.left === this) this.parent.left = null;
+        if (this.parent.right === this) this.parent.right = null;
       }
     }
   }
 
   find(key) {
     if (key === this.key) {
-      return this.value
+      return this.value;
     } else if (key < this.key) {
-      return this.left.find(key)
+      return this.left.find(key);
     } else if (key > this.key) {
-      return this.right.find(key)
+      return this.right.find(key);
     }
   }
   _findMin() {
     if (this.left === null) {
-      return this
+      return this;
     }
-    return this.left._findMin()
+    return this.left._findMin();
   }
 }
 
-module.exports = BinarySearchTree
+module.exports = BinarySearchTree;
--- drills.js
+++ drills.js
@@ -1,4 +1,5 @@
-const BinarySearchTree = require('./BinarySearchTree')
+const BinarySearchTree = require('./BinarySearchTree');
+const Queue = require('./Queues');
 
 // 1.
 
@@ -12,7 +13,6 @@ const BinarySearchTree = require('./BinarySearchTree')
 // 12, 14, 15, 17, 18
 // 17, 18
 // it would return null
-
 
 //3.
 function findBook(lib, dewey, title, start = 0, end = lib.length) {
@@ -35,8 +35,8 @@ function findBook(lib, dewey, title, start = 0, end = lib.length) {
 
 //4.
 // 1)
-// in-order: 14 15 19 25 27 35 79 89 90 91 and 
-// pre-order: 35 25 15 14 19 27 89 79 91 90. 
+// in-order: 14 15 19 25 27 35 79 89 90 91 and
+// pre-order: 35 25 15 14 19 27 89 79 91 90.
 // post-order: 14, 19, 15, 27, 25, 79, 90, 91, 89, 35
 
 //post-order: 5, 7, 6, 9, 11, 10, 8
@@ -44,59 +44,83 @@ function findBook(lib, dewey, title, start = 0, end = lib.length) {
 
 //5.
 const BST = new BinarySearchTree();
-BST.insert({key: 25, value: null})
-BST.insert({key: 15, value: null})
-BST.insert({key: 50, value: null})
-BST.insert({key: 10, value: null})
-BST.insert({key: 24, value: null})
-BST.insert({key: 35, value: null})
-BST.insert({key: 70, value: null})
-BST.insert({key: 4, value: null})
-BST.insert({key: 12, value: null})
-BST.insert({key: 18, value: null})
-BST.insert({key: 31, value: null})
-BST.insert({key: 44, value: null})
-BST.insert({key: 66, value: null})
-BST.insert({key: 90, value: null})
-BST.insert({key: 22, value: null})
-// console.log(BST)
+BST.insert({ key: 25, value: null });
+BST.insert({ key: 15, value: null });
+BST.insert({ key: 50, value: null });
+BST.insert({ key: 10, value: null });
+BST.insert({ key: 24, value: null });
+BST.insert({ key: 35, value: null });
+BST.insert({ key: 70, value: null });
+BST.insert({ key: 4, value: null });
+BST.insert({ key: 12, value: null });
+BST.insert({ key: 18, value: null });
+BST.insert({ key: 31, value: null });
+BST.insert({ key: 44, value: null });
+BST.insert({ key: 66, value: null });
+BST.insert({ key: 90, value: null });
+BST.insert({ key: 22, value: null });
+//console.log(BST);
 
-const preOrder = (tree) => {
-  console.log(tree.key)
+const preOrder = tree => {
+  console.log(tree.key);
   if (tree.left) {
-    preOrder(tree.left)
+    preOrder(tree.left);
   }
   if (tree.right) {
-    preOrder(tree.right)
+    preOrder(tree.right);
   }
-}
-const postOrder = (tree) => {
+};
+const postOrder = tree => {
   if (tree.left) {
-    postOrder(tree.left)
+    postOrder(tree.left);
   }
   if (tree.right) {
-    postOrder(tree.right)
+    postOrder(tree.right);
   }
-  console.log(tree.key)
-}
-const inOrder = (tree) => {
+  console.log(tree.key);
+};
+const inOrder = tree => {
   if (tree.left) {
-    inOrder(tree.left)
+    inOrder(tree.left);
   }
-  console.log(tree.key)
+  console.log(tree.key);
   if (tree.right) {
-    inOrder(tree.right)
+    inOrder(tree.right);
   }
-}
+};
 
-console.log('pre-order')
-preOrder(BST)
-console.log('in-order')
-inOrder(BST)
-console.log('post-order')
-postOrder(BST)
-
-
+// console.log('pre-order')
+// preOrder(BST)
+// console.log('in-order')
+// inOrder(BST)
+// console.log('post-order')
+// postOrder(BST)
 
 //25 15 50 10 24 35 70 4 12 18 31 44 66 90 22
 
+const officers = new BinarySearchTree();
+officers.insert({ key: 10, value: 'Captain Picard' });
+officers.insert({ key: 8, value: 'Commander Riker' });
+officers.insert({ key: 6, value: 'Lt. Cmdr. Worf' });
+officers.insert({ key: 9, value: 'Lt. Cmdr. LaForge' });
+officers.insert({ key: 5, value: 'Lieutenant security-officer' });
+officers.insert({ key: 11, value: 'Commander Data' });
+officers.insert({ key: 13, value: 'Lt. Cmdr. Crusher' });
+officers.insert({ key: 12, value: 'Lieutenant Selar' });
+
+const queue = new Queue();
+
+function nextOfficer(tree) {
+  if (tree.left) {
+    queue.enqueue(tree.left);
+  }
+  if (tree.right) {
+    queue.enqueue(tree.right);
+  }
+  console.log(tree.value);
+  if (queue.first) {
+    nextOfficer(queue.dequeue());
+  }
+}
+nextOfficer(officers);
+//console.log(officers);
