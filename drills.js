72c24a8b960c695b0c3a7bcde7f3f051e235ea38
Hector Valles <hmvalles75@yahoo.com>
2019-10-09 09:17:46 -0700

    3

--- drills.js
+++ drills.js
@@ -1,4 +1,4 @@
-// 1. 
+// 1.
 
 // 3, 5, 6, 8, 11, 12, 14, 15, 17, 18
 // 3, 5, 6, 8
@@ -10,3 +10,21 @@
 // 12, 14, 15, 17, 18
 // 17, 18
 // it would return null
+
+function findBook(lib, dewey, title, start = 0, end = lib.length) {
+  while (start < end) {
+    let mid = Math.floor((start + end) / 2);
+    if (lib[mid].dewey === dewey) {
+      if (lib[mid].title === title) {
+        return lib[mid];
+      }
+    }
+    if (lib[mid].dewey > dewey) {
+      findBook(lib, dewey, title, start, mid - 1);
+    }
+    if (lib[mid].dewey < dewey) {
+      findBook(lib, dewey, title, mid + 1, end);
+    }
+  }
+  return 'Book not found';
+}
